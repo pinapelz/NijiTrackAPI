6582f52d90f57eec28734e560e22b066a41f8bb7
Pinapelz <donaldshan1@outlook.com>
2023-04-04 11:38:43 -0700

    Updated response parameters

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ Returns the subcount closest to the iso_date
 """
 
 from flask import Flask, jsonify, send_from_directory, abort, request
+from flask_cors import CORS
 from sql_handler import SQLHandler
 import fileutil as fs
 import response as res
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/subs/<channel_id>')
@@ -18,15 +20,15 @@ def current_subs(channel_id):
     server = SQLHandler(hostname, username, password, database)
     if timestamp == "now":
         data = server.get_current_subscriber_count(channel_id)
+        response = res.subs_current_response(channel_id, data)
     else:
         table_key = "channel_"+channel_id.lower()+"_subscriber_data"
         table_key = table_key.replace("-", "$")
         data = server.get_subcount(table_key, timestamp)
+        response = res. subs_historical_response(channel_id, data)
     if data is None:
         abort(404, description="No data found for the given channel.")
-    response = res.subs_current_response(channel_id, data)
     return jsonify(response)
-
 
 @app.route('/rank/<channel_id>')
 def rank(channel_id):
